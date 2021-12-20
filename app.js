27389ddf81fcfbc7f3451729ce7db6f9d9dc1be0
SyscilyBrown <55116854+SyscilyBrown@users.noreply.github.com>
2021-12-20 07:03:44 -0600

    add v2 of working color script

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ function randomRGB() {
 const r = Math.floor(Math.random() * 256); 
 const g = Math.floor(Math.random() * 256); 
 const b = Math.floor(Math.random() * 256); 
-return `rbg(${r},${g},${b} )`
+return `rgb(${r},${g},${b})`
 }
 
 //to change each letter independtly 
 const letters = document.querySelectorAll(".letter")
-const intervalId = setInterval(function(){
+const intervalId = setInterval(function() {
     for (let letter of letters) {
         letter.style.color = randomRGB();
     }
